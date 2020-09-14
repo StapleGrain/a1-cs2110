fa59a1dc69398d816720890e8f14794f42cb0d07
StapleGrain <63561992+StapleGrain@users.noreply.github.com>
2020-09-14 11:44:57 -0400

    Update PhD.java

--- a1/src/a1/PhD.java
+++ a1/src/a1/PhD.java
@@ -1,7 +1,11 @@
 package a1;
-
+/** NetID: ekp38, . Time spent: hours, minutes.
+    What I thought about this assignment:
+    An instance maintains info about the PhD of a person. */
 public class PhD {
+  /** the name of the person with a PhD */
+  private String name;
   
-  raegbugarbioarwinowaf
+  
 
 }
