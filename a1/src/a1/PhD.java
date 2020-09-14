fc18c4aab2f36244ce656e6e8755e1b7eab98dec
StapleGrain <63561992+StapleGrain@users.noreply.github.com>
2020-09-14 11:55:10 -0400

    Update PhD.java

--- a1/src/a1/PhD.java
+++ a1/src/a1/PhD.java
@@ -3,11 +3,15 @@ package a1;
     What I thought about this assignment:
     An instance maintains info about the PhD of a person. */
 public class PhD {
-  /** the (String) name of the person with a PhD */
+  /** the (String) name of the person with a PhD, length > 0 */
   private String name;
-    
+
   /** the (int) month PhD was awarded. int 1 ... 12, January being 1, etc. */
-  private 
+  private Integer month;
+
+  /** the (int) year PhD was awarded. Must be >1000. */
+  private Integer year;
   
+
 
 }
